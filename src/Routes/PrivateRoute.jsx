d49e73946b233e208a6643b0a72f2965b2419145
soriful15 <sorifulislam.rs10@gmailcomm>
2023-08-26 05:21:13 +0600

    private route recheck

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,8 +1,6 @@
-
+import React, {  useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../Provider/AuthProvider';
-import { useContext } from 'react';
-
 
 const PrivateRoute = ({children}) => {
     const { user, loading } = useContext(AuthContext)
@@ -17,6 +15,5 @@ const PrivateRoute = ({children}) => {
 
     return <Navigate to='/login' state={{ from: location }} replace></Navigate>
 };
-
 
 export default PrivateRoute;
